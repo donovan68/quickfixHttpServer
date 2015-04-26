a6a16bec85b27128e969183b8f253ebe328e8225
Ankit Gupta <ankithbti@Ankits-MacBook-Pro.local>
2015-04-27 00:12:39 +0530

    Correcting gtest

--- httpServer/gtest/AddTest.cpp
+++ httpServer/gtest/AddTest.cpp
@@ -1,15 +1,11 @@
-#include <Add.hpp>
 #include <gtest/gtest.h>
 
 TEST(AddTest, Positive){
     EXPECT_EQ(1,1);
 }
 
-TEST(AddTest, Negative){
-    EXPECT_EQ(1,1);
-}
-
-TEST(AddTest, NewPositive)
+int main(int argc, char** argv)
 {
-    EXPECT_TRUE(true);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
