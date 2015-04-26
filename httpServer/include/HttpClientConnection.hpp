9441a8e0213b0bd53c4c6819fce3d0043243f5fb
ankithbti <ankithbti@192.168.1.5>
2015-04-26 22:06:01 +0530

    Updating the code base to add client connection.

--- httpServer/include/HttpClientConnection.hpp
+++ httpServer/include/HttpClientConnection.hpp
@@ -10,21 +10,38 @@
 
 #include <boost/shared_ptr.hpp>
 #include <boost/noncopyable.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
 
 namespace httpServer
 {
+class HttpClientConnManager;
 
-class HttpClientConnection : private boost::noncopyable
+class HttpClientConnection : private boost::noncopyable, public boost::enable_shared_from_this<HttpClientConnection>
 {
 public:
     typedef boost::shared_ptr<HttpClientConnection> SmartPtr;
     typedef boost::shared_ptr<const HttpClientConnection> ConstSmartPtr;
+    typedef boost::shared_ptr<boost::asio::ip::tcp::socket> SocketSmartPtr;
 private:
+    SocketSmartPtr _socket;
+    boost::array<char, 8192> _buffer;
+    std::string _clientId;
+    
+    boost::shared_ptr<HttpClientConnManager> _clientManager;
+    //HttpReqHandler::SmartPtr _reqHandler;
 
+    /// Perform an asynchronous read operation.
+    void do_read();
+
+    /// Perform an asynchronous write operation.
+    void do_write();
 public:
 
-    HttpClientConnection();
+    HttpClientConnection(SocketSmartPtr socket, boost::shared_ptr<HttpClientConnManager> manager/*, HttpReqHandler::Ptr reqHanlder*/);
 
+    void start();
+    void stop();
 
 };
 
--- httpServer/src/HttpClientConnection.cpp
+++ httpServer/src/HttpClientConnection.cpp
@@ -1,1 +1,33 @@
 #include <HttpClientConnection.hpp>
+
+namespace httpServer
+{
+
+HttpClientConnection::HttpClientConnection(SocketSmartPtr socket, boost::shared_ptr<HttpClientConnManager> manager) :
+_socket(socket),
+_clientManager(manager)
+{
+
+}
+
+void HttpClientConnection::do_read()
+{
+
+}
+
+void HttpClientConnection::do_write()
+{
+
+}
+
+void HttpClientConnection::start()
+{
+    do_read();
+}
+
+void HttpClientConnection::stop()
+{
+    _socket->close();
+}
+
+}
